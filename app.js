1c45a0feac4964c3513162ae9f5be72e54fa367c
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2016-05-29 16:14:16 -0300

    Configured Express to work with Ejs and structured partials

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/app/views');
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/app/views/index.html');
+    res.render('pages/index');
 });
 
 app.get('/branding', function(req, res) {
-    res.sendFile(__dirname + '/app/views/branding.html');
+    res.render('pages/branding');
 });
 
-var server = app.listen(process.env.PORT, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
+var server = app.listen(8080, function() {
+    console.log('Running app at http://localhost:8080');
 });
