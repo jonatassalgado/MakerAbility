be3ad3bd5eb9e13d9ea9c42b18284bd6f33d0a70
jonataseduardo <jonatas.salgado@acad.pucrs.br>
2016-09-09 10:51:45 -0300

    Configurado task para minificar css

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,33 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 var pump = require('pump');
 
-gulp.task('compress', function (cb) {
+gulp.task('compress', function(cb){
+  gulp.start('compress-js', 'compress-css');
+})
+
+gulp.task('compress-js', function (cb) {
   pump([
-        gulp.src(['public/js/vendor/*.js', 'public/js/components/*.js', 'public/js/application.js']),
-        concat('main.js'),
-        uglify(),
-        gulp.dest('public/js')
-    ],
-    cb
-  );
+    gulp.src(['public/js/vendor/*.js', 'public/js/components/*.js', 'public/js/application.js']),
+    concat('main.js'),
+    uglify(),
+    gulp.dest('public/js')
+  ],
+  cb
+);
+});
+
+gulp.task('compress-css', function (cb) {
+  pump([
+    gulp.src(['public/css/vendor/*.css', 'public/css/components/*.css', 'public/css/application/*.css']),
+    concat('main.css'),
+    cssmin(),
+    rename({suffix: '.min'}),
+    gulp.dest('public/css')
+  ],
+  cb
+);
 });
